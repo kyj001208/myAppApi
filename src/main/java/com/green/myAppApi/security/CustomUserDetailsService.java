3d8a8532e3788adf0508a6705fe5461ae7248550
kyj <kyj001208@naver.com>
2024-09-27 11:35:53 +0900

    feat

--- src/main/java/com/green/myAppApi/security/CustomUserDetailsService.java
+++ src/main/java/com/green/myAppApi/security/CustomUserDetailsService.java
@@ -5,13 +5,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.green.myAppApi.domain.entity.MemberEntityRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service  //인증 처리하는 서비스
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService{
+	
+	private final MemberEntityRepository repository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		log.info("---------------------loadUserByUsername-------------------");
 		
-		return null;
+		return new MemberDTO(repository.findByEmail(username).orElseThrow());
 	}
 
 }
